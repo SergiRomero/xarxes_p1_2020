398a94f0914de35eee3bcb605b650006d54e688f
SergiRomero <sergi9rom@gmail.com>
2020-10-17 22:03:51 +0200

    Split logic into explicit functions

--- xat_client.py
+++ xat_client.py
@@ -1,11 +1,13 @@
 import socket
-import select
 import threading
 
 
-IP = "192.168.1.48"
-PORT = 1234
+IP = "192.168.1.48"     # Server ip 
+PORT = 1234             # Server Port
 
+"""
+    Waits for user input and sends the socket to server
+"""
 def write_new_message(client_socket, username):
     while True:
         new_message = input()
@@ -14,6 +16,9 @@ def write_new_message(client_socket, username):
             client_socket.send(str.encode(new_message))
 
 
+"""
+    Checks if recived socket and prints it if recived
+"""
 def printRecivedMessages(client_socket):
     while True:
         try:
@@ -21,19 +26,22 @@ def printRecivedMessages(client_socket):
             print(message.decode())
         
         except IOError:
+            # There is no message to read
             pass
 
 
 
-
+# Identifies user on chat room
 username = input("Username: ")
 
+#Init socket
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect((IP, PORT))
 client_socket.setblocking(False) # Dont block connection
 
 client_socket.send(str.encode(username)) # Encode and send username to stablish connection
 
+# Create two threads to  paralelize send and recive message
 write_thread = threading.Thread(target=write_new_message, args=(client_socket, username))
 read_thread = threading.Thread(target=printRecivedMessages, args=(client_socket,))
 
--- xat_server.py
+++ xat_server.py
@@ -1,9 +1,99 @@
 import socket
 import select
 
-IP = "192.168.1.48"
-PORT = 1234
+IP = "192.168.1.48"     # Server ip
+PORT = 1234             # Server port
 
+"""
+    Recives and returns a message from specified socket
+"""
+def recive_message (client_socket):
+    try:
+        message = client_socket.recv(1024)
+        return message
+    except:
+        return False
+
+"""
+    Register a new client. Stores socket to resend messages and client data
+"""
+def save_new_client(scoket, username):
+    
+    sockets_list.append(scoket)
+    clients[scoket] = username # Save client data
+    print("Accepted new conection from ", clients[scoket])
+
+"""
+    Removes given client from registered sockets and client data
+"""
+def unsubscribe_client(client_socket):
+    
+    print("Close conection from " + clients[client_socket])
+    sockets_list.remove(client_socket)
+    del clients[client_socket]
+
+
+"""
+    Message view to be returned to clients
+"""
+def out_message(client_socket, message):
+
+    return clients[recived_socket] + ": " + message
+
+"""
+    Sens a given message to all clients except for a given (who send the message)
+"""
+def resend_to_all_clients(recived_socket, message):
+
+    for client_socket in clients:
+        if client_socket != recived_socket:
+            client_socket.send(str.encode(out_message(recived_socket, message)))
+
+
+
+
+
+
+"""
+    Accepts socket and register new client
+"""
+def handle_new_client(recived_socket):
+
+    client_socket, client_address = server_socket.accept()
+    message = recive_message(client_socket)
+
+    if not message:
+        return
+
+    save_new_client(client_socket, message.decode())
+
+
+"""
+    Reads recieved message and resends it to all clients
+"""
+def handle_message(recived_socket):
+    message = recive_message(recived_socket)
+
+    if not message:
+        unsubscribe_client(recived_socket)
+        return
+    
+    print("Recived message from " + out_message(recived_socket, message.decode()))
+
+    # Send recived message to other clients
+    resend_to_all_clients(recived_socket, message.decode())
+
+
+
+
+
+"""
+=================================================================
+
+                            MAIN
+
+=================================================================
+"""
 
 #Init socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -18,65 +108,20 @@ print("Listening on ", (IP, PORT))
 sockets_list = [server_socket]
 clients = {} #{socket: user_data}
 
-def recive_message (client_socket):
-    try:
-        message = client_socket.recv(1024) #Harcoded lenght
-
-        # TODO: If no data do something
-
-        return message
-    except:
-        return False
-
 while True:
+
     read_sockets, _, exception_sockets = select.select(sockets_list, [], [])
     
-    for s in read_sockets:
-
-        if s == server_socket:
-            client_socket, client_address = server_socket.accept() # Socket de subscripció. Utilitza el socket que hem obert per establir la comunicació
-            message = recive_message(client_socket)
-
-            if not message:
-                continue
-
-            sockets_list.append(client_socket) # Register new client
-            clients[client_socket] = message.decode()
-
-            print("Accepted new conection from ", message.decode())
+    for recived_socket in read_sockets:
+    
+        if recived_socket == server_socket:
+            handle_new_client(recived_socket)
 
         else:
-            message = recive_message(s)
+            handle_message(recived_socket)
+    
 
-            if not message:
-                print("Close conection from someone") # TODO: Provide decent feedback
-                sockets_list.remove(s)
-                del clients[s]
-                continue
-            
-            print("Recived message from " + clients[s] + ": " + message.decode())
-
-            for client_socket in clients:
-                if client_socket != s:
-                    out_text = clients[s] + ": " + message.decode()
-                    client_socket.send(str.encode(out_text))
-
+    # If error ocurred close socket with client
     for s in exception_sockets:
-        sockets_list.remove(s)
-        del clients[s]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        unsubscribe_client(recived_socket)
 
